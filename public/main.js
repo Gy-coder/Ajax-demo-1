59db0fe79804732283506afadbf525478f4b78b0
Gy <406631892@qq.com>
2020-02-14 15:00:34 +0800

    gitignore

---
+++ public/main.js
@@ -0,0 +1,70 @@
+getHTML.onclick = () => {
+    const request = new XMLHttpRequest
+    request.open('get', '/3.html')
+    request.onload = () => {
+        const div = document.createElement('div')
+        div.innerHTML = request.response
+        document.body.appendChild(div)
+    }
+    request.onerror = () => { }
+    request.send()
+}
+
+
+
+getJS.onclick = () => {
+    console.log(1)
+    const request = new XMLHttpRequest
+    request.open('GET', '/2.js')
+    request.onload = () => {
+        const script = document.createElement('script')
+        script.innerHTML = request.response
+        document.body.appendChild(script)
+    }
+    request.onerror = () => {
+        console.log('失败了')
+    }
+    request.send()
+}
+getCSS.onclick = () => {
+    const request = new XMLHttpRequest()
+    request.open('GET', "/style.css")
+    request.onreadystatechange = () => {
+        if (request.readyState === 4) {
+            if (request.status >= 200 && request.status < 300) {
+                const style = document.createElement('style')
+                style.innerHTML = request.response
+                document.head.appendChild(style)
+            } else {
+                alert('加载失败')
+            }
+        }
+    }
+    request.send()
+}
+getJSON.onclick = () => {
+    const request = new XMLHttpRequest()
+    request.open("GET", "/5.json")
+    request.onreadystatechange = () => {
+        if (request.readyState === 4 && request.status === 200) {
+            console.log(request.response)
+            const object = JSON.parse(request.response)
+            console.log(object)
+            myName.textContent = object[`name`]
+        }
+    }
+    request.send()
+}
+
+getXML.onclick = () => {
+    const request = new XMLHttpRequest()
+    request.open('GET', '/4.xml')
+    request.onreadystatechange = () => {
+        if (request.readyState === 4 && request.status === 200) {
+            const dom = request.responseXML
+            const text = dom.getElementsByTagName('warning')[0].textContent
+            console.log(text.trim())
+        }
+    }
+    request.send()
+}
